e89180020164af8072311fbe685a62e8875adc2e
calee14 <caplee1000@gmail.com>
2017-09-24 09:41:27 -0700

    working on finishing template

--- careersearch/javascript/jobs.js
+++ careersearch/javascript/jobs.js
@@ -1,24 +1,3 @@
-/* INFO WRAPPER JAVASCRIPT */
-
-$('.more').click(function(){
-    $(this).animate({'opacity': '0'}, 1000);
-    $('.less').animate({
-        'opacity': '1', 
-        'z-index': '1'
-    }, 1000);
-    $('.info').animate({'height': '15em'}, 1000);
-});
-$('.less').click(function(){
-    $(this).animate({
-        'opacity': '0',
-        'z-index': '-1'
-    }, 1000);
-    $('.more').animate({'opacity': '1'}, 1000);
-    $('.info').animate({'height': '120px'}, 1000);
-});
-
-/* END OF INFO WRAPPER JAVSCRIPT */
-
 /*NAV BAR SPACER */
 $(document).ready(function() {
   $(".container-info-wrapper").css("margin-top", $(".header").height());
