ce515442573a563c773ba0aafeb39cb4fe242b15
calee14 <caplee1000@gmail.com>
2017-10-28 14:39:56 -0700

    touching up ui

--- careersearch/javascript/index.js
+++ careersearch/javascript/index.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
   $(".mainNav-bar").css("margin-top", $(".header").height());
 });
 /* END OF NAV BAR SPACER */
+
+var holder = document.getElementsByClassName("category-holder");
+
+for (var i = 0; i < holder.length; i++) {
+    holder[i].addEventListener('mouseenter', menus, false);
+    holder[i].addEventListener('mouseleave', menus, false);
+}
+
+function menus() {
+  var menu = this;
+  menu.classList.toggle("category-shadow");
+};
