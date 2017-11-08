b9d31cb7b2ae8d6e58fde2f89378a960d2979914
calee14 <caplee1000@gmail.com>
2017-11-07 23:35:49 -0800

    finished todo list

--- todolist/index.js
+++ todolist/index.js
@@ -1,5 +1,23 @@
-var numArray = [];
-$("tr").each(function(n){
-   numArray[n] = $(this).text();
-});
-console.log(numArray)
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+$('#mytab1 tr').each(function(){
+    $(this).find('td').each(function(e){
+        //do your stuff, you can use $(this) to get current cell
+        if(e == 1) {
+        	var due = $(this).text();
+        	due = due.substring(due.indexOf("/") + 1);
+        	var priority = due - day
+            if(priority < 0) {
+                priority = "Hurry up and stop procrastinating";
+            }
+        	$(this).parent().children().eq(2).text(priority);
+        }
+        // console.log($(this))
+        // console.log(e);
+        // console.log($(this).text());
+        // console.log($(this).parent().children().eq(2));
+    })
+})
