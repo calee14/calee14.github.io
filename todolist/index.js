21638a947a266358df111f52cabebf58634f03d3
calee14 <caplee1000@gmail.com>
2017-11-08 18:16:44 -0800

    fixed the date calculations

--- todolist/index.js
+++ todolist/index.js
@@ -2,14 +2,26 @@ var d = new Date();
 
 var month = d.getMonth()+1;
 var day = d.getDate();
+var year = d.getFullYear();
+var today = month + '/' + day + '/' + year;
+
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2],mdy[0]-1,mdy[1]);
+}
+
+function daydiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
+console.log(daydiff(parseDate("11/9/2016"), parseDate("11/9/2017")));
 
 $('#mytab1 tr').each(function(){
     $(this).find('td').each(function(e){
         //do your stuff, you can use $(this) to get current cell
         if(e == 1) {
         	var due = $(this).text();
-        	due = due.substring(due.indexOf("/") + 1);
-        	var priority = due - day
+        	due = due.substring(due.indexOf(" ") + 1);
+        	var priority = daydiff(parseDate(today), parseDate(due));
             if(priority < 0) {
                 priority = "Hurry up and stop procrastinating";
             }
